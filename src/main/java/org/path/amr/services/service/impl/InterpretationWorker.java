2d5c8c3f67c9069acf9cbc7bf94b9945fd7f5504
Nguyen Huu Kien <hkien02@gmail.com>
2022-12-15 20:36:06 +0700

    ..

--- src/main/java/org/path/amr/services/service/impl/InterpretationWorker.java
+++ src/main/java/org/path/amr/services/service/impl/InterpretationWorker.java
@@ -3,9 +3,12 @@ package org.path.amr.services.service.impl;
 import java.util.concurrent.Callable;
 import org.path.amr.services.service.InterpretationService;
 import org.path.amr.services.service.dto.IsolateDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class InterpretationWorker implements Callable<IsolateDTO> {
 
+    private final Logger log = LoggerFactory.getLogger(InterpretationWorker.class);
     private InterpretationService interpretationService;
     private IsolateDTO isolateDTO;
 
@@ -16,7 +19,21 @@ public class InterpretationWorker implements Callable<IsolateDTO> {
 
     @Override
     public IsolateDTO call() throws Exception {
-        interpretationService.execute(isolateDTO);
+        try {
+            interpretationService.execute(isolateDTO);
+        } catch (Exception e) {
+            if (isolateDTO.getTest() != null && isolateDTO.getTest().size() > 0) {
+                for (int i = 0; i < isolateDTO.getTest().size(); i++) {
+                    log.error(
+                        "{} {} {}",
+                        isolateDTO.getOrgCode(),
+                        isolateDTO.getTest().get(i).getWhonet5Code(),
+                        isolateDTO.getTest().get(i).getRawValue()
+                    );
+                }
+            }
+            e.printStackTrace();
+        }
         return isolateDTO;
     }
 }
