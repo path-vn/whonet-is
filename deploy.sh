9f029d0bbf55253f452c3ce1b3111f4430ae79bf
Nguyen Huu Kien <hkien02@gmail.com>
2023-03-03 11:14:42 +0700

    add swagger

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,4 @@
 ./gradlew -Pprod,api-docs -Pswagger -Pwar clean bootWar
 
 docker build -t kks/interpretation:0.1.0 .
-
-docker save -o deploy/int.1.0.tar kks/interpretation:0.1.0
-
-scp deploy/int.1.0.tar root@kks:/tmp/
-
-rm deploy/int.1.0.tar
+ cd /opt/deploy/WHONET-is && docker-compose down && docker-compose up -d && cd -
