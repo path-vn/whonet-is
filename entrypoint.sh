9501fa1f5aab5c69cc6b6cabae79099b1a4b5844
Nguyen Huu Kien <hkien02@gmail.com>
2023-03-16 13:45:06 +0700

    Add docker aio

---
+++ entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+printenv | grep IS
+
+java ${JAVA_OPTS:-'-Xmx1g'} \
+    -Dspring.servlet.multipart.max-file-size=${IS_MAX_FILE_SIZE:-'50MB'} \
+    -Dspring.servlet.multipart.max-request-size=${IS_MAX_REQUEST_SIZE:-'100MB'} \
+    -Dspring.profiles.active=${IS_SPRING_PROFILES:-'prod,api-docs'} \
+    -Dspring.datasource.username=${IS_DATABASE_USERNAME:-'iis'} \
+    -Dspring.datasource.password=${IS_DATABASE_PASSWORD:-'iis@123'} \
+    -Dspring.liquibase.enabled=${IS_LIQUIBASE_ENABLE:-'false'} \
+    -Dwhonet.thread=${IS_WHONET_THREAD:-'10'} \
+    -Dwhonet.priority=${IS_WHONET_PRIORITY:-'Meningitis,Non-meningitis,Parenteral,Oral,,Urine,Intravenous,Extraintestinal'} \
+    -Dapplication.baseDirectory=${IS_BASE_DIRECTORY:-'/opt/data'} \
+    -Dspring.datasource.url=${IS_DATABASE:-'jdbc:mysql://localhost:3306/iis?useUnicode=true&characterEncoding=utf8&useSSL=false&allowPublicKeyRetrieval=true&useLegacyDatetimeCode=false&serverTimezone=UTC&createDatabaseIfNotExist=true'} \
+    -Dserver.port=${IS_PORT:-'8080'} \
+    -jar /app.jar ${0} ${@}
