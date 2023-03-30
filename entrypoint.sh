2b5d13b813828e3fb1caae90392870841abffbd3
Nguyen Huu Kien <hkien02@gmail.com>
2023-03-30 13:54:27 +0700

    Default pagging max is 5000000

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,5 +13,6 @@ java ${JAVA_OPTS:-'-Xmx1g'} \
     -Dwhonet.priority=${IS_WHONET_PRIORITY:-'Meningitis,Non-meningitis,Parenteral,Oral,,Urine,Intravenous,Extraintestinal'} \
     -Dapplication.baseDirectory=${IS_BASE_DIRECTORY:-'/opt/data'} \
     -Dspring.datasource.url=${IS_DATABASE:-'jdbc:mysql://localhost:3306/iis?useUnicode=true&characterEncoding=utf8&useSSL=false&allowPublicKeyRetrieval=true&useLegacyDatetimeCode=false&serverTimezone=UTC&createDatabaseIfNotExist=true'} \
+    -Dspring.data.rest.max-page-size=${REST_MAX_PAGE_SIZE:-'5000000'} \
     -Dserver.port=${IS_PORT:-'8080'} \
     -jar /app.jar ${0} ${@}
